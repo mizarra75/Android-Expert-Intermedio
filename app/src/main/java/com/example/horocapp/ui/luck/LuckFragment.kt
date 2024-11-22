449571a7681778031790619fa1009fe0725c6a49
mizarra <mizarra@gmail.com>
2024-11-22 12:46:11 -0400

    initial commit

---
+++ app/src/main/java/com/example/horocapp/ui/luck/LuckFragment.kt
@@ -0,0 +1,25 @@
+package com.example.horocapp.ui.luck
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.horocapp.R
+import com.example.horocapp.databinding.FragmentHoroscopeBinding
+import com.example.horocapp.databinding.FragmentLuckBinding
+
+
+class LuckFragment : Fragment() {
+    private var _binding: FragmentLuckBinding ? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentLuckBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+}
