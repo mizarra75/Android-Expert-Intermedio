d8548cbc05c4bff34cd356696a64e5130bd8443a
mizarra <mizarra@gmail.com>
2024-11-25 17:38:13 -0400

    include avance

--- app/src/main/java/com/example/horocapp/ui/detail/HoroscopeDetailActivity.kt
+++ app/src/main/java/com/example/horocapp/ui/detail/HoroscopeDetailActivity.kt
@@ -6,11 +6,17 @@ import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.isVisible
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.navArgs
 import com.example.horocapp.R
 import com.example.horocapp.databinding.ActivityHoroscopeDetailBinding
 import com.example.horocapp.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class HoroscopeDetailActivity : AppCompatActivity() {
@@ -18,11 +24,41 @@ class HoroscopeDetailActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHoroscopeDetailBinding
     private val horoscopeDetailViewModel: HoroscopeDetailViewModel by viewModels()
 
-    private val args:HoroscopeDetailActivityArgs by navArgs()
+    private val args: HoroscopeDetailActivityArgs by navArgs()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHoroscopeDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initUI()
+
+    }
+
+    private fun initUI() {
+        initUIState()
+    }
+
+    private fun initUIState() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                horoscopeDetailViewModel.state.collect {
+                    when (it) {
+                        is HoroscopeDetailState.Error -> errorState()
+                        HoroscopeDetailState.Loading -> loadingState()
+                        is HoroscopeDetailState.Success -> successState()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun loadingState() {
+        binding.pb.isVisible = true
+    }
+
+    private fun errorState() {
+
+    }
+    private fun successState() {
 
     }
 }
