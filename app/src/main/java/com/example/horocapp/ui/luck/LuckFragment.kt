287c5363ee576af04bb6d4849a98cb96a92b6704
mizarra <mizarra@gmail.com>
2024-11-27 18:42:58 -0400

    luck screen complete

--- app/src/main/java/com/example/horocapp/ui/luck/LuckFragment.kt
+++ app/src/main/java/com/example/horocapp/ui/luck/LuckFragment.kt
@@ -1,6 +1,7 @@
 package com.example.horocapp.ui.luck
 
 import android.animation.ObjectAnimator
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -13,28 +14,68 @@ import android.view.animation.AnimationUtils
 import android.view.animation.DecelerateInterpolator
 import androidx.core.animation.doOnEnd
 import androidx.core.view.isVisible
+import com.example.horocapp.ui.core.listeners.OnSwipeTouchListener
 import com.example.horocapp.R
-import com.example.horocapp.databinding.FragmentHoroscopeBinding
 import com.example.horocapp.databinding.FragmentLuckBinding
+import com.example.horocapp.ui.providers.RandomCardProvider
 import dagger.hilt.android.AndroidEntryPoint
 import java.util.Random
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class LuckFragment : Fragment() {
     private var _binding: FragmentLuckBinding? = null
     private val binding get() = _binding!!
 
+    @Inject
+    lateinit var randomCardProvider: RandomCardProvider
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initUI()
     }
 
     private fun initUI() {
+        preparePrediction()
         initListeners()
     }
 
+    private fun preparePrediction() {
+        val currentLuck = randomCardProvider.getLucky()
+        //La función let ejecuta el bloque de codigo si luk no es nulo
+        currentLuck?.let{luck->
+            val currentPrediction: String = getString(luck.text)
+            binding.tvLucky.text = currentPrediction
+            binding.ivLuckyCard.setImageResource(luck.image)
+            binding.tvShare.setOnClickListener {  shareResult(currentPrediction) }
+        }
+    }
+
+    private fun shareResult(prediction: String) {
+        val sendIntent : Intent = Intent().apply {
+            action = Intent.ACTION_SEND
+            putExtra(Intent.EXTRA_TEXT,prediction)
+            type = "text/plain"
+        }
+
+        val shareIntent = Intent.createChooser(sendIntent,null)
+        startActivity(shareIntent)
+    }
+
     private fun initListeners() {
-        binding.ivRoulette.setOnClickListener { spinRoulette() }
+        //Animación al hacer click
+        //binding.ivRoulette.setOnClickListener { spinRoulette() }
+
+        //Animaci'on a deslizar el dedo
+        binding.ivRoulette.setOnTouchListener(object : OnSwipeTouchListener(requireContext()){
+
+            override fun onSwipeRight() {
+                spinRoulette()
+            }
+
+            override fun onSwipeLeft() {
+                spinRoulette()
+            }
+        })
     }
 
     private fun spinRoulette() {
