a4bf1bd6ad96b2cf4c92d7a6193b82dbfe81b59c
mizarra <mizarra@gmail.com>
2024-11-28 09:51:33 -0400

    palmistry

--- app/src/main/java/com/example/horocapp/ui/palmistry/PalmistryFragment.kt
+++ app/src/main/java/com/example/horocapp/ui/palmistry/PalmistryFragment.kt
@@ -1,6 +1,7 @@
 package com.example.horocapp.ui.palmistry
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -8,6 +9,10 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContract
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
 import androidx.core.content.PermissionChecker
 import com.example.horocapp.R
 import com.example.horocapp.databinding.FragmentLuckBinding
@@ -17,37 +22,65 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class PalmistryFragment : Fragment() {
 
-    companion object{
+    companion object {
         private const val CAMERA_PERMISSION = android.Manifest.permission.CAMERA
     }
 
     private var _binding: FragmentPalmistryBinding? = null
     private val binding get() = _binding!!
     private val requestPermissionLauncher = registerForActivityResult(
-        ActivityResultContracts.RequestPermission()){
-        if(it){
+        ActivityResultContracts.RequestPermission()
+    ) {
+        if (it) {
+            startCamera()
 
-        }else{
-            Toast.makeText(requireContext(),"Debes aceptar los permisos",Toast.LENGTH_LONG).show()
+        } else {
+            Toast.makeText(requireContext(), "Debes aceptar los permisos", Toast.LENGTH_LONG).show()
         }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        if(checkCameraPermission()){
+        if (checkCameraPermission()) {
             //tiene permisos aceptados para la camara
-        }else {
+            startCamera()
+        } else {
             requestPermissionLauncher.launch(CAMERA_PERMISSION)
 
         }
 
     }
 
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
+
+        cameraProviderFuture.addListener({
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(binding.viewFinder.surfaceProvider)
+                }
+
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            try{
+                cameraProvider.unbindAll()
+
+                cameraProvider.bindToLifecycle(this, cameraSelector, preview)
+            }catch (e:Exception){
+                Log.e("aris", "Algo petó ${e.message}")
+            }
+        }, ContextCompat.getMainExecutor(requireContext()))
+    }
+
     private fun checkCameraPermission(): Boolean {
         return PermissionChecker.checkSelfPermission(
             requireContext(),
-            CAMERA_PERMISSION) == PermissionChecker.PERMISSION_GRANTED
+            CAMERA_PERMISSION
+        ) == PermissionChecker.PERMISSION_GRANTED
     }
 
     override fun onCreateView(
